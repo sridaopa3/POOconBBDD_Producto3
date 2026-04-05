669326b302f45dea9c51f7b5f72e4dbc81c369f4
sridaopa3 <sridaopa@uoc.edu>
2026-04-05 19:03:47 +0200

    P3: conexion, arquitectura DAO, Factory, resubido nueva rama (previamente borrada)

--- src/JDBC/ConexionBBDD.java
+++ src/JDBC/ConexionBBDD.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConexionBBDD {
     private static final String URL = "jdbc:mysql://localhost:3306/OnlineStore";
     private static final String USER = "root";
-    private static final String PASS = "SQLxUOC1421316_!";
+    private static final String PASS = "ESCRIBIR_CONTRASEÑA";
 
     public static Connection Conectar(){
         Connection conexion = null;
